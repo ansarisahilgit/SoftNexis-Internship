196e4369555040ea3508d2ad7e3b7fc542cdf874
ansarisahilgit <ansarisahilgit@gmail.com>
2026-06-10 20:09:29 +0530

    Refractor backend folder structure and integrated MongoDB Atlas

--- task-1-crud-api/backend/server.js
+++ task-1-crud-api/backend/server.js
@@ -1,12 +1,30 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
+
+const productRoutes = require("./routes/productRoutes");
 
 const app = express();
 
-const PORT = 5000;
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Server is running successfully");
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.use("/products", productRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+
+  res.status(500).json({
+    message: "Internal Server Error",
+  });
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
