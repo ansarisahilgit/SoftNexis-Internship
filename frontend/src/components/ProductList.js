955a3cbf4a15c769f74f26f15943d3aa74fcdccf
ansarisahilgit <ansarisahilgit@gmail.com>
2026-06-15 16:38:19 +0530

    feat: connect frontend with backend API

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -1,7 +1,32 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 function ProductList() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/products")
+      .then((response) => {
+        console.log(response.data);
+
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div>
-      <h2>Products Component</h2>
+      <h2>Products</h2>
+
+      {products.map((product) => (
+        <div key={product._id}>
+          <h3>{product.name}</h3>
+          <p>Price: {product.price}</p>
+        </div>
+      ))}
     </div>
   );
 }
