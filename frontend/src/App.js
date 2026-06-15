2643dfd5a71ac1c2302800a8beedd103296d2e33
ansarisahilgit <ansarisahilgit@gmail.com>
2026-06-15 19:04:05 +0530

    feat: add product creation functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import ProductList from "./components/ProductList";
+import AddProduct from "./components/AddProduct";
+
 function App() {
   return (
     <div>
       <h1>MERN Frontend Started</h1>
       <ProductList />
+      <AddProduct />
     </div>
   );
 }
---
+++ frontend/src/components/AddProduct.js
@@ -0,0 +1,60 @@
+import { useState } from "react";
+import axios from "axios";
+
+function AddProduct() {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:3000/products", {
+        name,
+        price,
+      })
+      .then((response) => {
+        console.log(response.data);
+
+        alert("Product Added Successfully");
+
+        setName("");
+        setPrice("");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <div>
+      <h2>Add Product</h2>
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter Product Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <br />
+        <br />
+
+        <input
+          type="number"
+          placeholder="Enter Product Price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+
+        <br />
+        <br />
+
+        <button type="submit">Add Product</button>
+      </form>
+    </div>
+  );
+}
+
+export default AddProduct;
